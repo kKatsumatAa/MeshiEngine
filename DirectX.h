e721254572645c752d742f21a99c168266883ce0
LC1A_04_Yuki_iida <k021g1333@g.neec.ac.jp>
2022-04-30 13:57:24 +0900

    03_02途中

--- DirectX.h
+++ DirectX.h
@@ -191,6 +191,107 @@ public:
 			result = device->CreateFence(fenceVal, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence));
 	}
 
+	void DrawInitialize()
+	{
+		// 頂点データ全体のサイズ = 頂点データ1つ分のサイズ * 頂点データの要素数
+		UINT sizeVB = static_cast<UINT>(sizeof(XMFLOAT3) * _countof(vertices));
+
+		//頂点バッファの設定
+		D3D12_HEAP_PROPERTIES heapProp{};			//ヒープ設定
+		heapProp.Type = D3D12_HEAP_TYPE_UPLOAD;		//GPUへの転送用
+		//リソース設定
+		D3D12_RESOURCE_DESC resDesc{};
+		resDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+		resDesc.Width = sizeVB;						//頂点データ全体のサイズ
+		resDesc.Height = 1;
+		resDesc.DepthOrArraySize = 1;
+		resDesc.MipLevels = 1;
+		resDesc.SampleDesc.Count = 1;
+		resDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+		//頂点バッファの生成
+		ID3D12Resource* vertBuff = nullptr;
+		result = device->CreateCommittedResource(
+			&heapProp, // ヒープ設定
+			D3D12_HEAP_FLAG_NONE,
+			&resDesc, // リソース設定
+			D3D12_RESOURCE_STATE_GENERIC_READ,
+			nullptr,
+			IID_PPV_ARGS(&vertBuff));
+		assert(SUCCEEDED(result));
+
+		// GPU上のバッファに対応した仮想メモリ(メインメモリ上)を取得
+		XMFLOAT3* vertMap = nullptr;
+		result = vertBuff->Map(0, nullptr, (void**)&vertMap);
+		assert(SUCCEEDED(result));
+		// 全頂点に対して
+		for (int i = 0; i < _countof(vertices); i++) {
+			vertMap[i] = vertices[i]; // 座標をコピー
+		}
+		// 繋がりを解除
+		vertBuff->Unmap(0, nullptr);
+
+		// 頂点バッファビューの作成
+		// GPU仮想アドレス
+		vbView.BufferLocation = vertBuff->GetGPUVirtualAddress();
+		// 頂点バッファのサイズ
+		vbView.SizeInBytes = sizeVB;
+		// 頂点1つ分のデータサイズ
+		vbView.StrideInBytes = sizeof(XMFLOAT3);
+
+
+		// 頂点シェーダの読み込みとコンパイル
+		result = D3DCompileFromFile(
+			L"BasicVS.hlsl", // シェーダファイル名
+			nullptr,
+			D3D_COMPILE_STANDARD_FILE_INCLUDE, // インクルード可能にする
+			"main", "vs_5_0", // エントリーポイント名、シェーダーモデル指定
+			D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, // デバッグ用設定
+			0,
+			&vsBlob, &errorBlob);
+
+		// エラーなら
+		Error(FAILED(result));
+
+		// ピクセルシェーダの読み込みとコンパイル
+		result = D3DCompileFromFile(
+			L"BasicPS.hlsl", // シェーダファイル名
+			nullptr,
+			D3D_COMPILE_STANDARD_FILE_INCLUDE, // インクルード可能にする
+			"main", "ps_5_0", // エントリーポイント名、シェーダーモデル指定
+			D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, // デバッグ用設定
+			0,
+			&psBlob, &errorBlob);
+
+		// エラーなら
+		Error(FAILED(result));
+
+		// パイプランステートの生成
+		PipeLineState(D3D12_FILL_MODE_SOLID, pipelineState);
+
+		PipeLineState(D3D12_FILL_MODE_WIREFRAME, pipelineState+1);
+
+
+		//03_02
+		//定数バッファ用データ構造体（マテリアル）
+		struct ConstBufferDataMaterial
+		{
+			XMFLOAT4 color;//色(RGBA)
+		};
+		//ヒープ設定
+		D3D12_HEAP_PROPERTIES cbHeapProp{};
+		cbHeapProp.Type = D3D12_HEAP_TYPE_UPLOAD;//GPUへの転送用
+		//リソース設定
+		D3D12_RESOURCE_DESC cbResourceDesc{};
+		cbResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+		cbResourceDesc.Width = (sizeof(ConstBufferDataMaterial) + 0xff) & ~0xff;//256バイトアライメント
+		cbResourceDesc.Height = 1;
+		cbResourceDesc.DepthOrArraySize = 1;
+		cbResourceDesc.MipLevels = 1;
+		cbResourceDesc.SampleDesc.Count = 1;
+		cbResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+	}
+
 	void GraphicsCommand(const XMFLOAT4& winRGBA)
 	{
 		// バックバッファの番号を取得(2つなので0番か1番)
@@ -244,112 +345,7 @@ public:
 		assert(SUCCEEDED(result));
 	}
 
-	void DrawInitialize()
-	{
-		// 頂点データ全体のサイズ = 頂点データ1つ分のサイズ * 頂点データの要素数
-		UINT sizeVB = static_cast<UINT>(sizeof(XMFLOAT3) * _countof(vertices));
-
-		//頂点バッファの設定
-		D3D12_HEAP_PROPERTIES heapProp{};			//ヒープ設定
-		heapProp.Type = D3D12_HEAP_TYPE_UPLOAD;		//GPUへの転送用
-		//リソース設定
-		D3D12_RESOURCE_DESC resDesc{};
-		resDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
-		resDesc.Width = sizeVB;						//頂点データ全体のサイズ
-		resDesc.Height = 1;
-		resDesc.DepthOrArraySize = 1;
-		resDesc.MipLevels = 1;
-		resDesc.SampleDesc.Count = 1;
-		resDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
-
-		//頂点バッファの生成
-		ID3D12Resource* vertBuff = nullptr;
-		result = device->CreateCommittedResource(
-			&heapProp, // ヒープ設定
-			D3D12_HEAP_FLAG_NONE,
-			&resDesc, // リソース設定
-			D3D12_RESOURCE_STATE_GENERIC_READ,
-			nullptr,
-			IID_PPV_ARGS(&vertBuff));
-		assert(SUCCEEDED(result));
-
-		// GPU上のバッファに対応した仮想メモリ(メインメモリ上)を取得
-		XMFLOAT3* vertMap = nullptr;
-		result = vertBuff->Map(0, nullptr, (void**)&vertMap);
-		assert(SUCCEEDED(result));
-		// 全頂点に対して
-		for (int i = 0; i < _countof(vertices); i++) {
-			vertMap[i] = vertices[i]; // 座標をコピー
-		}
-		// 繋がりを解除
-		vertBuff->Unmap(0, nullptr);
-
-		// 頂点バッファビューの作成
-		// GPU仮想アドレス
-		vbView.BufferLocation = vertBuff->GetGPUVirtualAddress();
-		// 頂点バッファのサイズ
-		vbView.SizeInBytes = sizeVB;
-		// 頂点1つ分のデータサイズ
-		vbView.StrideInBytes = sizeof(XMFLOAT3);
-
-		
-		// 頂点シェーダの読み込みとコンパイル
-		result = D3DCompileFromFile(
-			L"BasicVS.hlsl", // シェーダファイル名
-			nullptr,
-			D3D_COMPILE_STANDARD_FILE_INCLUDE, // インクルード可能にする
-			"main", "vs_5_0", // エントリーポイント名、シェーダーモデル指定
-			D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, // デバッグ用設定
-			0,
-			&vsBlob, &errorBlob);
-
-		// エラーなら
-		if (FAILED(result)) {
-			// errorBlobからエラー内容をstring型にコピー
-			std::string error;
-			error.resize(errorBlob->GetBufferSize());
-			std::copy_n((char*)errorBlob->GetBufferPointer(),
-				errorBlob->GetBufferSize(),
-				error.begin());
-			error += "\n";
-			// エラー内容を出力ウィンドウに表示
-			OutputDebugStringA(error.c_str());
-			assert(0);
-		}
-
-		// ピクセルシェーダの読み込みとコンパイル
-		result = D3DCompileFromFile(
-			L"BasicPS.hlsl", // シェーダファイル名
-			nullptr,
-			D3D_COMPILE_STANDARD_FILE_INCLUDE, // インクルード可能にする
-			"main", "ps_5_0", // エントリーポイント名、シェーダーモデル指定
-			D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, // デバッグ用設定
-			0,
-			&psBlob, &errorBlob);
-
-		// エラーなら
-		if (FAILED(result)) {
-			// errorBlobからエラー内容をstring型にコピー
-			std::string error;
-			error.resize(errorBlob->GetBufferSize());
-			std::copy_n((char*)errorBlob->GetBufferPointer(),
-				errorBlob->GetBufferSize(),
-				error.begin());
-			error += "\n";
-			// エラー内容を出力ウィンドウに表示
-			OutputDebugStringA(error.c_str());
-			assert(0);
-		}
-
-		// パイプランステートの生成
-		PipeLineState(D3D12_FILL_MODE_SOLID);
-		result = device->CreateGraphicsPipelineState(&pipelineDesc, IID_PPV_ARGS(&pipelineState[0]));
-		assert(SUCCEEDED(result));
-
-		PipeLineState(D3D12_FILL_MODE_WIREFRAME);
-		result = device->CreateGraphicsPipelineState(&pipelineDesc, IID_PPV_ARGS(&pipelineState[1]));
-		assert(SUCCEEDED(result));
-	}
+	
 
 	void DrawUpdate(const WindowsApp& win, const D3D12_VIEWPORT& viewPort, const int& pipelineNum,
 		const bool& primitiveMode)
@@ -388,7 +384,7 @@ public:
 	}
 
 
-	void PipeLineState(const D3D12_FILL_MODE& fillMode/*, const ID3D12PipelineState* pipelineState*/)
+	void PipeLineState(const D3D12_FILL_MODE& fillMode, ID3D12PipelineState** pipelineState)
 	{
 		// シェーダーの設定
 		pipelineDesc.VS.pShaderBytecode = vsBlob->GetBufferPointer();
@@ -433,6 +429,9 @@ public:
 		rootSigBlob->Release();
 		// パイプラインにルートシグネチャをセット
 		pipelineDesc.pRootSignature = rootSignature;
+
+		result = device->CreateGraphicsPipelineState(&pipelineDesc, IID_PPV_ARGS(pipelineState));
+		assert(SUCCEEDED(result));
 	}
 
 	void Blend(const D3D12_BLEND_OP& blendMode,
@@ -461,6 +460,23 @@ public:
 		{
 			blendDesc.SrcBlend = D3D12_BLEND_ONE;//ソースの値を100%使う
 			blendDesc.DestBlend = D3D12_BLEND_ONE;//デストの値を100%使う
+		}
+	}
+
+	void Error(const bool& filed)
+	{
+		if (filed)
+		{
+			// errorBlobからエラー内容をstring型にコピー
+			std::string error;
+			error.resize(errorBlob->GetBufferSize());
+			std::copy_n((char*)errorBlob->GetBufferPointer(),
+				errorBlob->GetBufferSize(),
+				error.begin());
+			error += "\n";
+			// エラー内容を出力ウィンドウに表示
+			OutputDebugStringA(error.c_str());
+			assert(0);
 		}
 	}
 };
