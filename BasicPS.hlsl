0da7b8b42c326567a03c39757a5243a5d2a4c352
LC1A_04_Yuki_iida <k021g1333@g.neec.ac.jp>
2022-05-19 12:01:12 +0900

    完了

--- BasicPS.hlsl
+++ BasicPS.hlsl
@@ -6,7 +6,6 @@ SamplerState smp : register(s0);      //0番スロットに設定されたサンプラー
 
 float4 main(VSOutput input) : SV_TARGET
 {
-	return tex.Sample(smp, input.uv);
-	//return float4(1,1,1,1);
+	return tex.Sample(smp, input.uv)*color;
 }
 
--- DirectX.h
+++ DirectX.h
@@ -328,7 +328,7 @@ public:
 		//定数バッファのマッピング
 		result = constBuffMaterial->Map(0, nullptr, (void**)&constMapMaterial);//マッピング
 		assert(SUCCEEDED(result));
-		constBuffTransfer({ 1.0f,0,0,0.5f });
+		constBuffTransfer({ 1.0f,0,0,1.0f });
 
 		//03_04
 		//インデックスデータ
@@ -357,49 +357,100 @@ public:
 		ibView.SizeInBytes = sizeIB;
 
 		//04_02
-		//横方向ピクセル数
-		const size_t textureWidth = 256;
-		//縦方向ピクセル数
-		const size_t textureHeight = 256;
-		//配列の要素数
-		const size_t imageDataCount = textureWidth * textureHeight;
-		//画像イメージデータ配列
-		XMFLOAT4* imageData = new XMFLOAT4[imageDataCount];//あとで必ず解放！！
-		//全ピクセルの色を初期化
-		for (size_t i = 0; i < imageDataCount; i++)
+		////横方向ピクセル数
+		//const size_t textureWidth = 256;
+		////縦方向ピクセル数
+		//const size_t textureHeight = 256;
+		////配列の要素数
+		//const size_t imageDataCount = textureWidth * textureHeight;
+		////画像イメージデータ配列
+		//XMFLOAT4* imageData = new XMFLOAT4[imageDataCount];//あとで必ず解放！！
+		////全ピクセルの色を初期化
+		//for (size_t i = 0; i < imageDataCount; i++)
+		//{
+		//	imageData[i].x = 1.0f;//R
+		//	imageData[i].y = 0.0f;//G
+		//	imageData[i].z = 0.0f;//B
+		//	imageData[i].w = 1.0f;//A
+		//}
+		
+		// 04_03
+		TexMetadata metadata{};
+		ScratchImage scratchImg{};
+		//WICのテクスチャのロード
+		result = LoadFromWICFile(
+			L"Resources/texture.jpg",
+			WIC_FLAGS_NONE,
+			&metadata, scratchImg
+		);
+
+		ScratchImage mipChain{};
+		//mipmap生成
+		result = GenerateMipMaps(
+			scratchImg.GetImages(), scratchImg.GetImageCount(), scratchImg.GetMetadata(),
+			TEX_FILTER_DEFAULT, 0, mipChain);
+		if (SUCCEEDED(result))
 		{
-			imageData[i].x = 1.0f;//R
-			imageData[i].y = 0.0f;//G
-			imageData[i].z = 0.0f;//B
-			imageData[i].w = 1.0f;//A
+			scratchImg = std::move(mipChain);
+			metadata = scratchImg.GetMetadata();
 		}
+		//読み込んだディフューズテクスチャをSRGBとして扱う
+		metadata.format = MakeSRGB(metadata.format);
+
 		//ヒープ設定
 		D3D12_HEAP_PROPERTIES textureHeapProp{};
 		textureHeapProp.Type = D3D12_HEAP_TYPE_CUSTOM;
 		textureHeapProp.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_WRITE_BACK;
 		textureHeapProp.MemoryPoolPreference = D3D12_MEMORY_POOL_L0;
+		////リソース設定
+		//D3D12_RESOURCE_DESC textureResourceDesc{};
+		//textureResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+		//textureResourceDesc.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+		//textureResourceDesc.Width = textureWidth;						//頂点データ全体のサイズ
+		//textureResourceDesc.Height = textureHeight;
+		//textureResourceDesc.DepthOrArraySize = 1;
+		//textureResourceDesc.MipLevels = 1;
+		//textureResourceDesc.SampleDesc.Count = 1;
+		// 04_03
 		//リソース設定
 		D3D12_RESOURCE_DESC textureResourceDesc{};
 		textureResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
-		textureResourceDesc.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
-		textureResourceDesc.Width = textureWidth;						//頂点データ全体のサイズ
-		textureResourceDesc.Height = textureHeight;
-		textureResourceDesc.DepthOrArraySize = 1;
-		textureResourceDesc.MipLevels = 1;
+		textureResourceDesc.Format = metadata.format;
+		textureResourceDesc.Width = metadata.width;						//頂点データ全体のサイズ
+		textureResourceDesc.Height = (UINT)metadata.height;
+		textureResourceDesc.DepthOrArraySize = (UINT16)metadata.arraySize;
+		textureResourceDesc.MipLevels = (UINT16)metadata.mipLevels;
 		textureResourceDesc.SampleDesc.Count = 1;
 		//テクスチャバッファの生成
 		ID3D12Resource* texBuff = nullptr;
 		BuffProperties(textureHeapProp, textureResourceDesc, &texBuff);
-		//テクスチャバッファにデータ転送
-		result = texBuff->WriteToSubresource(
-			0,
-			nullptr,//全領域へコピー
-			imageData,//元データアドレス
-			sizeof(XMFLOAT4) * textureWidth,//1ラインサイズ
-			sizeof(XMFLOAT4) * imageDataCount//全サイズ
-		);
-		//元データ解放
-		delete[] imageData;
+		////テクスチャバッファにデータ転送
+		//result = texBuff->WriteToSubresource(
+		//	0,
+		//	nullptr,//全領域へコピー
+		//	imageData,//元データアドレス
+		//	sizeof(XMFLOAT4) * textureWidth,//1ラインサイズ
+		//	sizeof(XMFLOAT4) * imageDataCount//全サイズ
+		//);
+		
+		//04_03
+		// 全ミップマップについて
+		for (size_t i = 0; i < metadata.mipLevels; i++)
+		{
+			//ミップマップレベルを指定してイメージを取得
+			const Image* img = scratchImg.GetImage(i, 0, 0);
+			//テクスチャバッファにデータ転送
+			result = texBuff->WriteToSubresource(
+				(UINT)i,
+				nullptr,//全領域へコピー
+				img->pixels,//元データアドレス
+				(UINT)img->rowPitch,//1ラインサイズ
+				(UINT)img->slicePitch//全サイズ
+			);
+			assert(SUCCEEDED(result));
+		}
+		////元データ解放
+		//delete[] imageData;
 
 		//SRVの最大個数
 		const size_t kMaxSRVCount = 2056;
@@ -415,12 +466,22 @@ public:
 		D3D12_CPU_DESCRIPTOR_HANDLE srvHandle = srvHeap->GetCPUDescriptorHandleForHeapStart();
 		
 		//シェーダーリソースビュー設定
+		//D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc{};//設定構造体
+		//srvDesc.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+		//srvDesc.Shader4ComponentMapping =
+		//	D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+		//srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;//2Dテクスチャ
+		//srvDesc.Texture2D.MipLevels = 1;
+		////ハンドルのさす位置にシェーダーリソースビュー作成
+		//device->CreateShaderResourceView(texBuff, &srvDesc, srvHandle);
+		
+		//04_03
 		D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc{};//設定構造体
-		srvDesc.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+		srvDesc.Format = resDesc.Format;
 		srvDesc.Shader4ComponentMapping =
 			D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
 		srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;//2Dテクスチャ
-		srvDesc.Texture2D.MipLevels = 1;
+		srvDesc.Texture2D.MipLevels = resDesc.MipLevels;
 		//ハンドルのさす位置にシェーダーリソースビュー作成
 		device->CreateShaderResourceView(texBuff, &srvDesc, srvHandle);
 	}
