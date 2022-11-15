c7639a23e862ad6698fbd23c02406f47257fe780
LE2A_04_Yuki_iida <k021g1333@g.neec.ac.jp>
2022-11-15 11:35:00 +0900

    修正

--- Scene.cpp
+++ Scene.cpp
@@ -162,7 +162,8 @@ void Scene::Draw(UINT64* textureHandle, UINT64* textureNumHundle)
 	//scale += 0.01f;
 	uvwidth += 0.005f;
 	color += 0.005f;
-	//draw[5].DrawClippingBoxSprite({ 0,0,0 }, scale, { 0,0.2f }, { uvwidth,0.8f }, { 0,color,0,1.0f }, texhandle[1], false, rot);
+	draw[5].DrawClippingBoxSprite({ 0,0,0 }, scale, { 0,0.2f }, { uvwidth,0.8f }, { 0,color,0,1.0f }, texhandle[1], false, rot);
+	draw[8].DrawBoxSprite({ 100,500,0 }, scale,{ 1.0f,color,0,1.0f }, texhandle[1]);
 	draw[6].DrawCube3D(draw[6].worldMat, &viewMat, &projectionMat, { 1.0f,0.0f,1.0f,1.0f }, texhandle[0]);
 	draw[7].DrawLine(draw[7].worldMat, &viewMat, &projectionMat, { 1.0f,1.0f,1.0f,1.0f }, texhandle[0]);
 
