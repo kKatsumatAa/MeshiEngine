b4f8640755f988c0982f1741730be547fc27b96b
LE2A_04_Yuki_iida <k021g1333@g.neec.ac.jp>
2023-02-16 22:15:12 +0900

    テクスチャバッファをカスタムからデフォルトに変更

--- TextureManager.cpp
+++ TextureManager.cpp
@@ -1,4 +1,6 @@
 #include "TextureManager.h"
+#include <d3dx12.h>
+using namespace DirectX;
 
 int TextureManager::count = 0;
 //リソース設定
@@ -90,73 +92,146 @@ void TextureManager::LoadGraph(const wchar_t* name, UINT64& textureHandle)
 	//読み込んだディフューズテクスチャをSRGBとして扱う
 	metadata.format = MakeSRGB(metadata.format);
 
-	//ヒープ設定
-	D3D12_HEAP_PROPERTIES textureHeapProp{};
-	textureHeapProp.Type = D3D12_HEAP_TYPE_CUSTOM;
-	textureHeapProp.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_WRITE_BACK;
-	textureHeapProp.MemoryPoolPreference = D3D12_MEMORY_POOL_L0;
-	// 04_03
 
-	//リソース設定
-	D3D12_RESOURCE_DESC textureResourceDesc{};
-	textureResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
-	textureResourceDesc.Format = metadata.format;
-	textureResourceDesc.Width = metadata.width;						//頂点データ全体のサイズ
-	textureResourceDesc.Height = (UINT)metadata.height;
-	textureResourceDesc.DepthOrArraySize = (UINT16)metadata.arraySize;
-	textureResourceDesc.MipLevels = (UINT16)metadata.mipLevels;
-	textureResourceDesc.SampleDesc.Count = 1;
+	HRESULT result;
 
-	//テクスチャバッファの生成
-	//ID3D12Resource* texBuff = nullptr;
-	Directx::GetInstance().result = Directx::GetInstance().GetDevice()->CreateCommittedResource(
-		&textureHeapProp,//ヒープ設定
-		D3D12_HEAP_FLAG_NONE,
-		&textureResourceDesc,//リソース設定
-		D3D12_RESOURCE_STATE_GENERIC_READ,
-		nullptr,
-		IID_PPV_ARGS(&TextureManager::GetInstance().texBuff[count]));
-	assert(SUCCEEDED(Directx::GetInstance().result));
+	//コピー先用
+	// ヒープの設定
+	CD3DX12_HEAP_PROPERTIES textureHeapProp =
+		CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT);
 
-	//04_03
-	// 全ミップマップについて
-	for (size_t i = 0; i < metadata.mipLevels; i++)
+	// リソース設定
+	CD3DX12_RESOURCE_DESC textureResourceDesc =
+		CD3DX12_RESOURCE_DESC::Tex2D(
+			metadata.format,
+			(UINT64)metadata.width,
+			(UINT)metadata.height,
+			(UINT16)metadata.arraySize,
+			(UINT16)metadata.mipLevels,
+			1);
+
+	// テクスチャバッファの生成
+	result = Directx::GetInstance().GetDevice()->
+		CreateCommittedResource(
+			&textureHeapProp,
+			D3D12_HEAP_FLAG_NONE,
+			&textureResourceDesc,
+			D3D12_RESOURCE_STATE_COPY_DEST,
+			nullptr,
+			IID_PPV_ARGS(&texBuff[count]));
+	assert(SUCCEEDED(result));
+
+
+	//アップロードバッファ
+	// ヒープの設定
+	uint64_t uploadSize = GetRequiredIntermediateSize(texBuff[count].Get(), 0, metadata.mipLevels);
+
+	D3D12_HEAP_PROPERTIES uploadHeapProp{};
+	uploadHeapProp.Type = D3D12_HEAP_TYPE_UPLOAD;
+	CD3DX12_RESOURCE_DESC resDesc =
+		CD3DX12_RESOURCE_DESC::Buffer(uploadSize);
+
+	Microsoft::WRL::ComPtr<ID3D12Resource> uploadBuff;
+
+	// テクスチャバッファの生成
+	result = Directx::GetInstance().GetDevice()->
+		CreateCommittedResource(
+			&uploadHeapProp,
+			D3D12_HEAP_FLAG_NONE,
+			&resDesc,
+			D3D12_RESOURCE_STATE_GENERIC_READ,
+			nullptr,
+			IID_PPV_ARGS(&uploadBuff));
+	assert(SUCCEEDED(result));
+
+
+	//サブリソース生成
+	std::vector<D3D12_SUBRESOURCE_DATA> subResourcesDatas{};
+	subResourcesDatas.resize(metadata.mipLevels);
+
+	for (size_t i = 0; i < subResourcesDatas.size(); i++)
 	{
-		//ミップマップレベルを指定してイメージを取得
+		// 全ミップマップレベルを指定してイメージを取得
 		const Image* img = scratchImg.GetImage(i, 0, 0);
-		//テクスチャバッファにデータ転送
-		Directx::GetInstance().result = texBuff[count]->WriteToSubresource(
-			(UINT)i,
-			nullptr,//全領域へコピー
-			img->pixels,//元データアドレス
-			(UINT)img->rowPitch,//1ラインサイズ
-			(UINT)img->slicePitch//全サイズ
-		);
-		assert(SUCCEEDED(Directx::GetInstance().result));
+
+		subResourcesDatas[i].pData = img->pixels;
+		subResourcesDatas[i].RowPitch = img->rowPitch;
+		subResourcesDatas[i].SlicePitch = img->slicePitch;
 	}
-	////元データ解放
-	//delete[] imageData;
+
+	//サブリソースを転送
+	UpdateSubresources(
+		Directx::GetInstance().GetCommandList(),
+		texBuff[count].Get(),
+		uploadBuff.Get(),
+		0,
+		0,
+		metadata.mipLevels,
+		subResourcesDatas.data()
+	);
+
+	//SRV
+	{
+		//SRVヒープの先頭ハンドルを取得
+		if (count == 0) { srvHandle = srvHeap->GetCPUDescriptorHandleForHeapStart(); }
+		else { srvHandle.ptr += Directx::GetInstance().GetDevice()->GetDescriptorHandleIncrementSize(srvHeapDesc.Type); }
+
+		//04_03
+		D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc{};//設定構造体
+		srvDesc.Format = resDesc.Format;
+		srvDesc.Shader4ComponentMapping =
+			D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+		srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;//2Dテクスチャ
+		srvDesc.Texture2D.MipLevels = 1;
+		//ハンドルのさす位置にシェーダーリソースビュー作成
+		Directx::GetInstance().GetDevice()->CreateShaderResourceView(TextureManager::GetInstance().texBuff[count].Get(), &srvDesc, srvHandle);
+
+		//04_02(画像貼る用のアドレスを引数に)
+		//SRVヒープのハンドルを取得
+		D3D12_GPU_DESCRIPTOR_HANDLE srvGpuHandle = srvHeap->GetGPUDescriptorHandleForHeapStart();
+		textureHandle = srvGpuHandle.ptr + (Directx::GetInstance().GetDevice()->GetDescriptorHandleIncrementSize(TextureManager::GetInstance().srvHeapDesc.Type) * count);
 
 
-	//SRVヒープの先頭ハンドルを取得
-	if (count == 0) { srvHandle = srvHeap->GetCPUDescriptorHandleForHeapStart(); }
-	else { srvHandle.ptr += Directx::GetInstance().GetDevice()->GetDescriptorHandleIncrementSize(srvHeapDesc.Type); }
+		//元データ解放
+		scratchImg.Release();
+	}
 
-	//04_03
-	D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc{};//設定構造体
-	srvDesc.Format = resDesc.Format;
-	srvDesc.Shader4ComponentMapping =
-		D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
-	srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;//2Dテクスチャ
-	srvDesc.Texture2D.MipLevels = 1;
-	//ハンドルのさす位置にシェーダーリソースビュー作成
-	Directx::GetInstance().GetDevice()->CreateShaderResourceView(TextureManager::GetInstance().texBuff[count].Get(), &srvDesc, srvHandle);
+	//バリアとフェンス
+	D3D12_RESOURCE_BARRIER BarrierDesc = {};
+	BarrierDesc.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+	BarrierDesc.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;
+	BarrierDesc.Transition.pResource = texBuff[count].Get();
+	BarrierDesc.Transition.Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES;
+	BarrierDesc.Transition.StateBefore = D3D12_RESOURCE_STATE_COPY_DEST;//ここが重要
+	BarrierDesc.Transition.StateAfter = D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE;//ここも重要
 
-	//04_02(画像貼る用のアドレスを引数に)
-	//SRVヒープのハンドルを取得
-	D3D12_GPU_DESCRIPTOR_HANDLE srvGpuHandle = srvHeap->GetGPUDescriptorHandleForHeapStart();
-	textureHandle = srvGpuHandle.ptr + (Directx::GetInstance().GetDevice()->GetDescriptorHandleIncrementSize(TextureManager::GetInstance().srvHeapDesc.Type) * count);
+	Directx::GetInstance().GetCommandList()->ResourceBarrier(1, &BarrierDesc);
+	Directx::GetInstance().GetCommandList()->Close();
+
+	//コマンドリストの実行
+	ID3D12CommandList* cmdlists[] = { Directx::GetInstance().GetCommandList() };
+	Directx::GetInstance().GetCommandQueue()->ExecuteCommandLists(1, cmdlists);
+
+	//コマンド閉じる
+	Directx::GetInstance().GetCommandQueue()->Signal(Directx::GetInstance().GetFence(),
+		++Directx::GetInstance().GetFenceVal());
+
+	if (Directx::GetInstance().GetFence()->GetCompletedValue() != Directx::GetInstance().GetFenceVal())
+	{
+		auto event = CreateEvent(nullptr, false, false, nullptr);
+		Directx::GetInstance().GetFence()->SetEventOnCompletion(Directx::GetInstance().GetFenceVal(), event);
+		WaitForSingleObject(event, INFINITE);
+		CloseHandle(event);
+	}
+
+	//バッファ解放
+	/*uploadBuff->Release();
+	uploadBuff.Reset();*/
+
+	//コマンドリセット
+	Directx::GetInstance().CommandReset();
 
 
+	//バッファ用のカウント
 	count++;
 }
